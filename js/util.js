e4d878c2a8bfc8d3a1f5fc3a605d9045be194c1a
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2023-05-16 12:31:09 +0500

    new backend service

--- js/util.js
+++ js/util.js
@@ -1,17 +1,17 @@
-const backendApi = 'https://nemomensshirts-xayrulloh.koyeb.app'
+const backendApi = "https://nodemailer-xayrulloh.koyeb.app/nemomensshirt"
 
 async function requestJSON(route, method, body) {
-    try {
-        let response = await fetch(backendApi + route, {
-            method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: body ? JSON.stringify(body): null
-        })
+  try {
+    let response = await fetch(backendApi + route, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body ? JSON.stringify(body) : null,
+    })
 
-        return await response.json()
-    } catch (error) {
-        console.log(error)
-    }
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+  }
 }
